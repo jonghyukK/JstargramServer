8effb978fec6f045944cbf1d44320a68a4003b16
mac <saz300@naver.com>
2019-04-28 23:18:47 +0900

    test

--- models/upload_images.js
+++ models/upload_images.js
@@ -2,9 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const upload_images = sequelize.define('upload_images', {
     file_path: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true
+      type: DataTypes.BLOB('long'),
+      allowNull: false
     },
     size: {
       type : DataTypes.DOUBLE,
